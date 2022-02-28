8db011683d45aade4a04e0085ac4dfcbb7ff7454
B Pavan kumar <62787933+pavankumar468@users.noreply.github.com>
2022-02-28 09:08:13 +0530

    Add files via upload

---
+++ positive_negative.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class positive_negative{
+    public static void main(String[] args) {
+        //Scanner sc =  new Scanner(System.in);
+        //int n = sc.nextInt();
+        int n=5;
+        if(n >0 ){
+            System.out.println("positive");
+        }else{
+            System.out.println("negative");
+        }
+
+
+    }
+
+}
